3e3bdd35eef07ef890495255ba45e978547d92a2
AntonDegt <108671823+AntonDegt@users.noreply.github.com>
2022-10-09 20:58:21 +0300

    Update TVFileWork.h

--- TVFileWork.h
+++ TVFileWork.h
@@ -5,10 +5,12 @@
 
 #include "TVschedule.h"
 
+// Пространство имён
 namespace TVFileWork
 {
-
+	// Запись в файл
 	void WriteToFile(TVschedule schedule, std::string path);
+	// Чтение из файла
 	TVschedule ReadFromFile(std::string path);
 
 }
